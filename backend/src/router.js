3721725188ad1bd155320d209334440fca41e631
Julien Marcenac <julien.marcenac2@gmail.com>
2023-02-06 10:10:46 +0100

    multer back done

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+// Ajout de uuid
+const { v4: uuidv4 } = require("uuid");
 
+const upload = multer({ dest: "uploads/" });
 const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
@@ -10,4 +15,22 @@ router.put("/items/:id", itemControllers.edit);
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
 
+// route post multer
+router.post("/api/avatar", upload.single("avatar"), (req, res) => {
+  // On récupère le nom du fichier
+  const { originalname } = req.file;
+
+  // On récupère le nom du fichier
+  const { filename } = req.file;
+
+  // On utilise la fonction rename de fs pour renommer le fichier
+  fs.rename(
+    `uploads/${filename}`,
+    `uploads/${uuidv4()}-${originalname}`,
+    (err) => {
+      if (err) throw err;
+      res.send("File uploaded");
+    }
+  );
+});
 module.exports = router;
